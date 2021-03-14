61ca01683285d95c1d8d92333d1bec486a2180ec
OhidulAlam <mdtasib442@gmail.com>
2021-03-14 17:33:02 +0600

    project

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,45 @@
-import './App.css';
+import React from 'react';
+import Header from '../Components/Header';
+import { newsCategory } from '../News/Index'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <h3 className='bg-info'>Hlleo</h3>
+const fakeNews = [
+  {
+    title: 'Title',
+    content: 'Content',
+    url: 'https://linktonews.com',
+    urlToImage: 'https://linktoimage.com',
+    publishedAt: 'published date and time',
+    source: {
+      name: 'CNN'
+    },
+  },
+  {
+    title: 'Title',
+    content: 'Content',
+    url: 'https://linktonews.com',
+    urlToImage: 'https://linktoimage.com',
+    publishedAt: 'published date and time',
+    source: {
+      name: 'CNN'
+    },
+  },
+]
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 offset-md-3">
+              <Header category={newsCategory.technology} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
